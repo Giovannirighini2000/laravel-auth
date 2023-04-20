5fd5b545f432320432bf350a1ef959268f1ce909
Giovannirighini2000 <righinigiovanni0@gmail.com>
2023-04-20 16:37:36 +0200

    feat added stamp project

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,7 +2,52 @@
 
 
 @section('content')
-    post index
+    <div class="container">
+        <div>
+            <h1>
+                I tuoi progetti
+            </h1>
+            <div>
+                <a class="btn" href="{{route('projects.create')}}">Nuovo progetto</a>
+            </div>
+        </div>
+        <div class="container">
+            <table class="table ">
+                <thead>
+                    <tr>
+                        <th>ID </th>
+                        <th>TITOLO</th>
+                        <th>SLUG</th>
+                        <th>DESCRIZIONE</th>
+                        <th>DATE</th>
+                        <th>URL</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($projects as $project)
+                    <tr>
+                        <td>{{$project->id}}</td>
+                        <td><a href="{{route('projects.show',$project)}}">{{$project->title}}</a></td>
+                        <td>{{$project->slug}}</td>
+                        <td>{{$project->description}}</td>
+                        <td>{{$project->date}}</td>
+                        <td>{{$project->url}}</td>
+                        <td>
+                            <a class="btn" href="{{route('projects.edit',$project)}}">MODIFICA</a>
+                        </td>
+                    </tr>
+                    @empty 
+                    <tr>
+                        <td>Vuoto</td>
+                    </tr>
+                    @endforelse
+                </tbody>
+
+            </table>
+
+        </div>
+
+    </div>
 
 
 @endsection
